33df3173790dbffd722e5befb37ea6197f097a91
julendiaz <iamjulendiaz@gmail.com>
2021-10-06 22:47:24 +0200

    Add about section with sample text

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './css/style.css';
 import { Navbar } from './components/Navbar';
 import { Jumbotron } from './components/Jumbotron';
 import { Stats } from './components/Stats';
+import { About } from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Navbar /> 
       <Jumbotron />
-      <Stats />
+      <Stats /> 
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div className="about">
+            <div className="about__text about__text--left">
+                <h1>Adding value<br/> through code<br/> and design</h1>
+                <p>Some of the companies I’ve worked with have been backed by YC, Techstarts, Seedcamp, Founders Factory and other top startup accelerators and VCs. My work has been featured on The Next Web, TechCrunch, Awwwards, Wired and other leading </p>
+            </div>
+            <div className="about__text about__text--right">
+                <p>Some of the companies I’ve worked with have been backed by YC, Techstarts, Seedcamp, Founders Factory and other top startup accelerators and VCs. My work has been featured on The Next Web, TechCrunch, Awwwards, Wired and other leading </p>
+                <p>Some of the companies I’ve worked with have been backed by YC, Techstarts, Seedcamp, Founders Factory and other top startup accelerators and VCs. My work has been featured on The Next Web, TechCrunch, Awwwards, Wired and other leading </p>
+                <p>Some of the companies I’ve worked with have been backed by YC, Techstarts, Seedcamp, Founders Factory and other top startup accelerators and VCs. My work has been featured on The Next Web, TechCrunch, Awwwards, Wired and other leading </p>
+            </div>
+        </div>
+    )
+}
