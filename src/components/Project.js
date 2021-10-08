49dea8b310ac3a5b0b89d9ce389797b6d0c768d8
julendiaz <iamjulendiaz@gmail.com>
2021-10-08 16:31:12 +0200

    Add styles for the project links buttons

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,8 +8,8 @@ export const Project = (props) => {
                 <h1 className="project__title">{props.title}</h1>
                 <p className="project__description">{props.text}</p>
                 <div className="project__links">
-                    <button className="button button--source">Source Code</button>
-                    <button className="button button--preview">Live Preview</button>
+                    <button className="button button--project">Source Code</button>
+                    <button className="button button--project">Live Preview</button>
                 </div>
             </div>
             <div className="project__division project__division--image">
