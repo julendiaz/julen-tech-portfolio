db5017848e85f5079bbf060769fd0cc9aa3ace0f
julendiaz <iamjulendiaz@gmail.com>
2021-10-08 17:45:37 +0200

    Add basic structure for the react component Project Card

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import { Stats } from './components/Stats';
 import { About } from './components/About';
 import { Project } from './components/Project';
 
+const superjob = {
+  title: "Superjob Tracker",
+  description: "Job Application Tracker with a Kanban Board for helping during the job hunt. This is a collaboration project between Nicolás Ríos and myself.",
+  stack: {
+      one: "🧬 React",
+      two: "📦️ Webpack",
+      three: "🖌️ Sass",
+  },
+  links: {
+      source: "https://github.com/julendiaz/superjob",
+      preview: "https://julendiaz.github.io/superjob/",
+  },
+  image: "https://user-images.githubusercontent.com/66780327/130353281-3aea537f-00b3-4099-8fdd-c8a18426b8ea.png",
+  alt: "Project Thumbnail for Superjob",
+}
+
 function App() {
   return (
     <div className="App"> 
@@ -13,7 +29,7 @@ function App() {
       <Jumbotron />
       <Stats /> 
       <About />
-      <Project title="Superjob Tracker" text="Job Application Tracker with a Kanban Board for helping during the job hunt. This is a collaboration project between Nicolás Ríos and myself."/>
+      <Project stack={superjob.stack} title={superjob.title} description={superjob.description} links={superjob.links} image={superjob.image} alt={superjob.alt} />
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,21 +5,20 @@ export const Project = (props) => {
         <div className="project project__container">
             <div className="project__division project__division--text">
                 <div className="project__stack">
-                    <button className="tag tag--stack">🧬 React</button>
-                    <button className="tag tag--stack">📦️ Webpack</button>
-                    <button className="tag tag--stack">🖌️ Sass</button>
+                    <button className="tag tag--stack">{props.stack.one}</button>
+                    <button className="tag tag--stack">{props.stack.two}</button>
+                    <button className="tag tag--stack">{props.stack.three}</button>
                 </div>
                 <h1 className="project__title">{props.title}</h1>
-                <p className="project__description">{props.text}</p>
+                <p className="project__description">{props.description}</p>
                 <div className="project__links">
-                    <button className="button button--project">Source Code</button>
-                    <button className="button button--project">Live Preview</button>
+                    <a href={props.links.source} className="button button--project">Source Code</a>
+                    <a href={props.links.preview} className="button button--project">Live Preview</a>
                 </div>
             </div>
             <div className="project__division project__division--image">
-               
+                <img src={props.image} alt={props.alt}/>
             </div>
         </div>
     )
-    
-}
+};
