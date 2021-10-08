7e7c16448e2b68e0e6f75977ed7e2a383d14efbe
julendiaz <iamjulendiaz@gmail.com>
2021-10-08 16:21:20 +0200

    Build the basic style for the project-card container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from './components/Navbar';
 import { Jumbotron } from './components/Jumbotron';
 import { Stats } from './components/Stats';
 import { About } from './components/About';
+import { Project } from './components/Project';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Jumbotron />
       <Stats /> 
       <About />
+      <Project title="Superjob Tracker" text="Job Application Tracker with a Kanban Board for helping during the job hunt. This is a collaboration project between Nicolás Ríos and myself."/>
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 export const Project = (props) => {
-    <div className="project project__container">
-        <div className="project__text">
-            <div className="project__stack"></div>
-            <h1 className="project__title">{props.title}</h1>
-            <p className="project__text">{props.text}</p>
-            <div className="project__links">
-                <button className="button button--source">Source Code</button>
-                <button className="button button--preview">Live Preview</button>
+    return (
+        <div className="project project__container">
+            <div className="project__division project__division--text">
+                <div className="project__stack"></div>
+                <h1 className="project__title">{props.title}</h1>
+                <p className="project__description">{props.text}</p>
+                <div className="project__links">
+                    <button className="button button--source">Source Code</button>
+                    <button className="button button--preview">Live Preview</button>
+                </div>
+            </div>
+            <div className="project__division project__division--image">
+               
             </div>
         </div>
-        <div className="project__image">
-            <img src="{props.image}" alt="{props.image-title}"></img>
-        </div>
-    </div>
+    )
+    
 }
